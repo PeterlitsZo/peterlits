c2c0188c579d291eeb48eb7b404a41bc58a63a2a
Peterlits Zo <peterlitszo@outlook.com>
2020-08-19 01:27:34 +0800

    test: (func test) update the first test about /user

--- backend/test/test.py
+++ backend/test/test.py
@@ -9,4 +9,18 @@ class TestAPI(unittest.TestCase):
         self.browser.quit()
 
     def test_use_API_1(self):
-        self.fail("Hello")
+        # Peter is me, and Peter want to access /user/list to get the list of
+        # user's list (by GET)
+        self.browser.get('http://localhost:8000/v1/user/list')
+
+        # Peter find that it return a JSON reponse
+        help(self.browser)
+        self.fail("assert that the return reponse is JSON")
+        self.fail("Finish test!")
+
+        # Peter want to access /user, but it return a error
+        self.browser.get('http://localhost:8000/v1/user')
+        self.fail('need get a error')
+
+        # Peter post a request with vaild session and it works
+        self.fail('Finish test!')
